a344fbcdafaebf2c0b15c40e75f98b6f817f2b39
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-09-09 19:15:09 +0300

    aded all the get top_*** end points, added the get song by name end point

--- server/app.js
+++ server/app.js
@@ -21,4 +21,36 @@ app.get("/top_songs",(req,response)=>{
     })
 })
 
+app.get("/top_artists",(req,response)=>{
+    let sql="SELECT * FROM artists LIMIT 20"
+    DataBase.query(sql,(err,res)=>{
+        if (err) throw err;
+        response.send(res)
+    })
+})
+
+app.get("/top_albums",(req,response)=>{
+    let sql="SELECT * FROM albums LIMIT 20"
+    DataBase.query(sql,(err,res)=>{
+        if (err) throw err;
+        response.send(res)
+    })
+})
+
+app.get("/top_playlists",(req,response)=>{
+    let sql="SELECT * FROM playlists LIMIT 20"
+    DataBase.query(sql,(err,res)=>{
+        if (err) throw err;
+        response.send(res)
+    })
+})
+
+app.get("/song/:title",(req,response)=>{
+    let sql=`SELECT * FROM songs WHERE title LIKE '${req.params.title}'`
+    DataBase.query(sql,(err,res)=>{
+        if (err) throw err;
+        response.send(res.length>0?res:"We couldn't find the song you were looking for")
+    })
+})
+
 module.exports=app
