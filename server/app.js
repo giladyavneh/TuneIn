81ad455c9a69c5086511418e11a6fb25b8aa0c50
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-09-09 18:23:24 +0300

    try to connect....

--- server/app.js
+++ server/app.js
@@ -2,5 +2,14 @@ const mysql=require("mysql")
 const express=require("express")
 const app=express()
 
+const DataBase=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"giladyavneh"
+})
+DataBase.connect(err=>{
+    if (err) throw err;
+    console.log("DataBase connected...")
+})
 
 module.exports=app
