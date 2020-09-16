373b9f8916351109af686b174cfe550866043e1a
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-09-17 00:43:47 +0300

    media player is a floating component, always exists, and can be closed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./Home"
 import MediaPlayer from "./MediaPlayer";
 function App(){
+    const [playingNow,setPlayingNow]=useState()
+    const [mediaPlays,setMediaPlays]=useState()
+    useEffect(()=>fetch("/top_songs").then(res=>res.json()).then(res=>setPlayingNow(res)),[])
+    useEffect(()=>setMediaPlays(true),[playingNow])
     return(
         <div>
-            <MediaPlayer/>
+            {mediaPlays?<MediaPlayer songs={playingNow} closePlayer={()=>setMediaPlays(false)}/>:""}
         </div>
     )
 }
--- client/src/MediaPlayer.js
+++ client/src/MediaPlayer.js
@@ -3,17 +3,15 @@ import './MediaPlayer.css';
 import PlayListPlayer from "./PlayListPlayer";
 import Player from "./Player";
 
-function MediaPlayer(props) {
+function MediaPlayer({songs,closePlayer}) {
   const noMoreSongs={
     title:"No song to be played",artist: "Tune in",album: "Keep it simple",length:"00:00:00",link:""
 }
   
   const [currentSong, setCurrentSong]=useState()
-  const [songs, setSongs]=useState()
+  // const [songs, setSongs]=useState()
   const [minimized,setMinimized]=useState()
-  useEffect(()=>{
-    fetch("/top_songs").then(res=>res.json()).then(res=>setSongs(res))
-},[])
+
 useEffect(()=>{if(songs) setCurrentSong(songs[0])},[songs])
   function play(key){
     let current=songs[key]||noMoreSongs
@@ -30,12 +28,14 @@ useEffect(()=>{if(songs) setCurrentSong(songs[0])},[songs])
     console.log(ind)
     play(ind-1)
   }
-  let topBar=<div id="playerTop"><button>x</button><button onClick={()=>setMinimized(true)}>-</button></div>
+  let topBar=<div id="playerTop"><button onClick={closePlayer}>x</button><button onClick={()=>setMinimized(true)}>-</button></div>
   return (
-    <div className={minimized?"minimized":"MediaPlayer"}>
-      {minimized?"":topBar}
-      {currentSong?<Player opener={()=>setMinimized(false)} minimized={minimized} previous={previous} song={currentSong} next={next}/>:""}
-      {minimized?"":<PlayListPlayer songs={songs} play={play}/>}
+    <div className={minimized?"minimized":"wrapper"}>
+      <div className={minimized?"minimized":"MediaPlayer"}>
+        {minimized?"":topBar}
+        {currentSong?<Player opener={()=>setMinimized(false)} minimized={minimized} previous={previous} song={currentSong} next={next}/>:""}
+        {minimized?"":<PlayListPlayer songs={songs} play={play}/>}
+      </div>
     </div>
   );
 }
