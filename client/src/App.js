977d2e7426ec83fef1c8882cd8631eb4f71d8990
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-09-17 01:00:38 +0300

    adjusting the player to fit the overall page

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import MediaPlayer from "./MediaPlayer";
 function App(){
     const [playingNow,setPlayingNow]=useState()
     const [mediaPlays,setMediaPlays]=useState()
-    useEffect(()=>fetch("/top_songs").then(res=>res.json()).then(res=>setPlayingNow(res)),[])
+    useEffect(async ()=>{let songs=await fetch("/top_songs").then(res=>res.json())
+setPlayingNow(songs)},[])
     useEffect(()=>setMediaPlays(true),[playingNow])
     return(
         <div>
+            <Home/>
             {mediaPlays?<MediaPlayer songs={playingNow} closePlayer={()=>setMediaPlays(false)}/>:""}
         </div>
     )
