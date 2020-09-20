2c738b82e8013fc9febe4eab5a5ade1909706a22
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-09-20 10:00:18 +0300

    add album page

---
+++ client/src/AddAlbum.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import "./AddSong.css";
+
+function AddAlbum() {
+    const [name,setName]=useState()
+    const [cover_image,setCoverImage]=useState()
+    const [artist,SetArtist]=useState()
+    const [artist_id,setArtistId]=useState()
+    const History=useHistory()
+    const [massage,setMassage]=useState()
+    function submit(e){
+        e.stopPropagation()
+        if(name){
+            let content={name, artist_id, cover_image}
+            let options={
+                method:"POST",
+                body:JSON.stringify(content),
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            }
+            fetch('/album', options).then(res=>History.push('/'))
+        }
+        else{
+            setMassage("A Name and an Artist must be submitted!")
+        }
+    }
+  return (
+    <div onClick={()=>setMassage("")}>
+      <div className="preview">
+        <div className="coverImage">
+          <img src={cover_image} />
+        </div>
+        <div className="info">
+          <h1 className="title"> {name} </h1>
+          <div className="meta">
+            <h2>{artist}</h2>
+          </div>
+        </div>
+      </div>
+      <div className="form">
+        <div className="fields">
+          <div className="songTitle">
+            <h4>Cover Image: </h4>
+            <input onChange={e=>setCoverImage(e.target.value)} className="plain"/>
+          </div>
+          <div className="songTitle">
+          <h4>Artist:</h4>
+          <SearchBar
+            style={{ width: "100%", height: "20px" }}
+            type={["artist"]}
+            onChoose={({title,id})=>{SetArtist(title);setArtistId(id)}}
+          />
+        </div>
+          <div className="songTitle">
+            <h4>Name: </h4>
+            <input onChange={e=>setName(e.target.value)} className="plain"/>
+          </div>
+        </div>
+        <div className="submitArea">
+            <div className="massageBoard">
+                {massage}
+            </div>
+            <div className="submitButton">
+                <button onClick={submit}>SUBMIT</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddAlbum;
--- client/src/AddContent.js
+++ client/src/AddContent.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AddAlbum from "./AddAlbum";
 import AddArtist from "./AddArtist";
 import "./AddContent.css";
 import AddSong from "./AddSong";
@@ -12,6 +13,11 @@ function AddContent(){
             break;
         case "artist":
             currentCard=<AddArtist/>
+            break;
+        case "album":
+            currentCard=<AddAlbum/>
+            console.log("albums")
+            break;
     }
     return(
         <div className="AddContent">
@@ -19,7 +25,7 @@ function AddContent(){
                 <div className="tabs">
                     <button onClick={()=>setCurrent("song")}>song</button>
                     <button onClick={()=>setCurrent("artist")}>artist</button>
-                    <button onClick={()=>{}}>album</button>
+                    <button onClick={()=>setCurrent("album")}>album</button>
                     <button onClick={()=>{}}>playlist</button>
                 </div>
                 <div className="content">
