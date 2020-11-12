727c0bb17126fd42d5ccf2f0ab9330ab9eb1430a
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-11-12 19:02:01 +0200

    added an update and a get route

--- server/elasticSearch.js
+++ server/elasticSearch.js
@@ -1,3 +1,32 @@
 const elasticSearch=require("@elastic/elasticsearch")
 
-const client=elasticSearch.Client()
+const client=new elasticSearch.Client({
+    node:"http://localhost:9200"
+})
+
+const updateElasticData = async(index, dataArray)=>{
+    await client.indices.create(
+        {
+          index
+        }
+      );
+      const body = dataArray.flatMap((doc) => [
+        { index: { _index: index } },
+        doc,
+      ]);
+      const { body: bulkResponse } = await client.bulk({ refresh: true, body });
+      if (bulkResponse.errors) {
+        console.log("ERROR");
+        return bulkResponse.errors;
+      } else {
+          const { body: count } = await client.count({ index: index });
+          console.log(count);
+        return bulkResponse;
+      }
+}
+
+const getElasticIndex = async (index)=>{
+    return await client.search({index, size:10000})
+}
+
+module.exports={updateElasticData, getElasticIndex}
